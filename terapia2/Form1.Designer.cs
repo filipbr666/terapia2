ef3b8fba6500d401eaba0c4514763884723e32d4
Filip Barnigiel <filipbr666@gmail.com>
2017-05-10 19:32:20 +0200

    Add project files.

---
+++ terapia2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace terapia2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Timer timer1;
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 78);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(188, 68);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 740);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ terapia2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+
+namespace terapia2
+{
+    public partial class Form1 : Form
+    {
+        int x1, x2, x3, x4;
+        int backcolor = 250;
+        int randomval = 0;
+        int volumevalue = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+            comboBox1.Items.AddRange(devices.ToArray());
+            x1 = 50;
+            x2 = 50;
+            x3 = 50;
+            x4 = 50;
+        }
+        Random rnd = new Random();
+        private NAudio.Wave.WaveFileReader wave = null;
+        private NAudio.Wave.DirectSoundOut output = null;
+        int firstcolor = 250;
+        int secondcolor, thirdcolor = 0;
+
+        int y1, y2, y3, y4;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (30 < volumevalue && volumevalue < 35)
+            {
+                x2 = 200;
+                if (x1 > 10) { x1 = x1 - 10; }
+                if (x3 > 10) { x3 = x3 - 10; }
+                if (x4 > 10) { x4 = x4 - 10; }
+            }
+            if (20 < volumevalue && volumevalue < 30)
+            {
+                x3 = 100;
+                x4 = 100;
+                if (x1 > 10) { x1 = x1 - 10; }
+                if (x2 > 10) { x2 = x2 - 10; }
+            }
+            if (10 < volumevalue && volumevalue < 20)
+            {
+                x3 = 200;
+                if (x1 > 10) { x1 = x1 - 10; }
+                if (x2 > 10) { x2 = x2 - 10; }
+                if (x4 > 10) { x4 = x4 - 10; }
+            }
+            if (50 < volumevalue && volumevalue < 55)
+            {
+                x1 = 200;
+                if (x3 > 10) { x3 = x3 - 10; }
+                if (x4 > 10) { x4 = x4 - 10; }
+                if (x3 > 10) { x3 = x3 - 10; }
+            }
+            if (60 < volumevalue && volumevalue < 80)
+            {
+                if (backcolor < 250) { backcolor++; }
+                else
+                {
+                    backcolor = 50;
+                    randomval = rnd.Next(50, 200);
+                    
+                }
+                this.BackColor = System.Drawing.Color.FromArgb(randomval, backcolor, 250 - backcolor);
+            }
+            else
+            {
+                if (x1 > 10) { x1 = x1 - 10; }
+                if (x3 > 10) { x3 = x3 - 10; }
+                if (x2 > 10) { x2 = x2 - 10; }
+                if (x4 > 10) { x4 = x4 - 10; }
+
+            }
+
+            /*
+            if (volumevalue < 30 && volumevalue > 1)
+            {
+                x1 = 200;
+                if (x3 > 10) x1 = x1 - 10;
+                if (x2 > 10) x3 = x3 - 10;
+                if (x4 > 10) x4 = x4 - 10;
+            }
+            else
+            {
+                x4 = 200;
+                if (x1 > 10) x1 = x1 - 10;
+                if (x2 > 10) x3 = x3 - 10;
+                if (x3 > 10) x4 = x4 - 10;
+            }*/
+            Invalidate();
+        }
+
+        int Height, Width = 100;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region INICJOWANIE DŹWIĘKU
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Wave File (*.wav)|*.wav;";
+            if (open.ShowDialog() != DialogResult.OK) return;
+
+            wave = new NAudio.Wave.WaveFileReader(open.FileName);
+            output = new NAudio.Wave.DirectSoundOut();
+            output.Init(new NAudio.Wave.WaveChannel32(wave));
+            output.Play();
+            #endregion
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                var device = (MMDevice)comboBox1.SelectedItem;
+                button1.Enabled = true;
+                volumevalue = (int)Math.Round(device.AudioMeterInformation.MasterPeakValue * 100);
+                progressBar1.Value = (int)(Math.Round(device.AudioMeterInformation.MasterPeakValue * 100));
+            }
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.Brown, 200, 300, x1, x1);
+            e.Graphics.FillEllipse(Brushes.GreenYellow, 400, 300, x2, x2);
+            e.Graphics.FillEllipse(Brushes.Yellow, 600, 300, x3, x3);
+            e.Graphics.FillEllipse(Brushes.Blue, 800, 300, x4, x4);
+            e.Graphics.FillEllipse(Brushes.Yellow, 600, 600, x3, x3);
+            e.Graphics.FillEllipse(Brushes.Blue, 300, 600, x3, x3);
+            e.Graphics.FillEllipse(Brushes.GreenYellow, 400, 800, x2, x2);
+            e.Graphics.FillEllipse(Brushes.Brown, 700, 700, x1, x1);
+        }
+    }
+}
